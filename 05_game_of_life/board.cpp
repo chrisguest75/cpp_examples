1b98298dd90d2cfd63ed60221d239c0084c805b5
Chris Guest <chris.guest@trint.com>
2022-02-13 22:18:00 +0000

    Adding tests for printing out state

--- 05_game_of_life/board.cpp
+++ 05_game_of_life/board.cpp
@@ -1,8 +1,11 @@
+#include <sstream>
+
 #include "board.hpp"
 
 Board::Board(int width, int height) {
     this->width = width;
     this->height = height;
+    this->board = new int[height * width];
 }
 
 int Board::getWidth() {
@@ -11,4 +14,22 @@ int Board::getWidth() {
 
 int Board::getHeight() {
     return this->height;
+}
+
+std::vector<std::string> Board::asString() {
+    std::vector<std::string> rows;
+
+    for (int y = 0; y < this->height; y++) {
+        std::stringstream row;
+        for (int x = 0; x < this->width; x++) {
+            if (this->board[(y * this->height) + x] == 0) {
+                row << " ";
+            } else {
+                row << "*";
+            }
+        }
+        rows.push_back(row.str());
+    }
+
+    return rows;
 }
--- 05_game_of_life/board_unittest.cpp
+++ 05_game_of_life/board_unittest.cpp
@@ -3,7 +3,6 @@
 
 #include "board.hpp"
 
-//our first unit test
 TEST(BoardTests, creation)
 {
   // ARRANGE
@@ -15,6 +14,23 @@ TEST(BoardTests, creation)
   EXPECT_EQ(board.getWidth(), 5) << "The width is not correct";
   EXPECT_EQ(board.getHeight(), 6) << "The height is not correct";
 }
+
+TEST(BoardTests, print)
+{
+  // ARRANGE
+  Board board(5, 5);
+
+  // ACT 
+  std::vector<std::string> rows = board.asString();
+
+  // ASSERT
+  EXPECT_EQ(board.getWidth(), 5) << "The width is not correct";
+  EXPECT_EQ(board.getHeight(), 5) << "The height is not correct";
+  EXPECT_EQ(rows.size(), 5) << "Number of rows is not correct";
+
+  
+}
+
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
